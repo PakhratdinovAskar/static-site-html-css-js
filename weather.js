201d3c656c7678058e2eec1a7c1ae6393c9d21da
Askar <112753402+PakhratdinovAskar@users.noreply.github.com>
2023-01-31 22:49:01 +0600

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,31 @@
+let apiKey = '5aa741a37ff6512516bcb3da3ea973f0'
+let city = 'Алматы'
+
+
+function find(){
+    let urlApi = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}`
+    $.ajax({
+        url: urlApi, 
+        dataType: 'json',       
+        method: 'get',           
+        success: function(data){
+
+            document.getElementById('city').innerHTML = data.name
+            document.getElementById('humidity').innerHTML = data.main.humidity
+            document.getElementById('wind').innerHTML = data.wind.speed
+
+            let t = Math.round(data.main.temp)
+            if (t > 0){
+                document.getElementById('temp').innerHTML = '+' + t
+            }else{
+                document.getElementById('temp').innerHTML = t
+            }
+        }
+    });
+}
+
+
+function findCity(){
+    city = document.getElementById('text').value
+    find()
+}
